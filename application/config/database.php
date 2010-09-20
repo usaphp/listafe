bab72b7f47d9164e61f8e95d671f47b10d7fa2d5
usaphp@gmail.com <usaphp@gmail.com>
2010-09-20 17:33:58 +0400

    asdsasad

--- application/config/database.php
+++ application/config/database.php
@@ -39,7 +39,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "snatch";
+$db['default']['password'] = "snatchs";
 $db['default']['database'] = "povarenok";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
