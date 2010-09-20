5cc5b444bc17af5343c4edc69e3375f4791f2c93
usaphp@gmail.com <usaphp@gmail.com>
2010-09-20 17:30:53 +0400

    asda

--- application/config/database.php
+++ application/config/database.php
@@ -39,7 +39,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
 $db['default']['username'] = "root";
-$db['default']['password'] = "";
+$db['default']['password'] = "snatch";
 $db['default']['database'] = "povarenok";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
