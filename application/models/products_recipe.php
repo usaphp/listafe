d7f664a77de9eede48d6e202cb37fe55e3bdd78c
Maksim From Tamik Laptop <strongvine@gmail.com>
2010-10-19 17:22:14 +0400

    adsas

---
+++ application/models/products_recipe.php
@@ -0,0 +1,10 @@
+<?php
+class Products_Recipe extends DataMapper {
+    
+    var $has_one = array('product','recipe');
+    function __construct($id = NULL)
+    {
+        parent::__construct($id);
+    }
+    
+}
