21d7a0bc6153169f01a57618ba07874e33b828c0
Maksim From Tamik Laptop <strongvine@gmail.com>
2010-10-25 10:00:29 +0400

    abrakadabra

--- application/config/config-example.php
+++ application/config/config-example.php
@@ -15,6 +15,8 @@ $config['base_url']	= "http://local.povarenok/";
 $config['admin_url'] = $config['base_url']."admin/";
 
 $config['product_images_path'] = $_SERVER['DOCUMENT_ROOT'].'/photos/product_images/';
+$config['recipe_images_path'] = $_SERVER['DOCUMENT_ROOT'].'/images/recipes/';
+$config['step_images_path'] = $_SERVER['DOCUMENT_ROOT'].'/images/steps/';
 /*
 |--------------------------------------------------------------------------
 | Index File
