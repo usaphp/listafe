f0938d7dfc975f67217bfb4d97e115d6b1fde099
Tamik Soziev <usaphp@gmail.com>
2010-11-11 01:54:10 +0300

    translate statuses sql added

---
+++ application/db/data_translate_statuses.sql
@@ -0,0 +1,22 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50141
+Source Host           : localhost:3306
+Source Database       : povarenok
+
+Target Server Type    : MYSQL
+Target Server Version : 50141
+File Encoding         : 65001
+
+Date: 2010-11-11 01:53:49
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Records of translate_statuses
+-- ----------------------------
+BEGIN;
+INSERT INTO `translate_statuses` VALUES ('1', 'новый'), ('2', 'переведён'), ('3', 'одобрен'), ('4', 'в процессе');
+COMMIT;
