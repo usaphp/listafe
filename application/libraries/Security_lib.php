41c0679eff4420ecf1e380a172bd0a579f41f099
Tamik Soziev <usaphp@gmail.com>
2010-11-11 22:14:13 +0300

    login changed

--- application/libraries/Security_lib.php
+++ application/libraries/Security_lib.php
@@ -24,7 +24,7 @@ class Security_lib {
 	public function login($username, $password){
 	   
 		$admin_user = new Admin_user();
-		if(!$admin_user->login($username, $password)){
+		if($admin_user->login($username, $password)){
 			$userdata = array('user_id' => $admin_user->id, 'logged_in' => TRUE);
 			$this->CI->session->set_userdata($userdata);
 			$this->update_login_status();
